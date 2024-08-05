59c0d974adc1a6c22c1b2cd147c53a286e55a117
𓆩•𝐊𝐢𝐬𝐬𝐮 💞•𓆪 <kissuXbots@gmail.com>
2024-08-05 18:59:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -54,7 +54,7 @@ class Bot(Client):
         tame = curr.strftime('%I:%M:%S %p')
         logger.info(LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__))
         try: await self.send_message(LOG_CHANNEL, text=LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__), disable_web_page_preview=True)   
-        except Exception as e: logger.warning(f"Bot Isn't Able To Send Message To LOG_CHANNEL \n{e}")
+        except Exception as e: logger.warning(f"Bot Is not Able To Send Message To LOG_CHANNEL \n{e}")
         if WEB_SUPPORT:
             app = web.AppRunner(await web_server())
             await app.setup()
