7731031d93fa32dfbefa6350c1a63cd22551f038
𓆩•𝐊𝐢𝐬𝐬𝐮 💞•𓆪 <kissuXbots@gmail.com>
2024-08-05 19:02:44 +0530

    Update connections_mdb.py

--- database/connections_mdb.py
+++ database/connections_mdb.py
@@ -133,3 +133,6 @@ async def delete_connection(user_id, group_id):
     except Exception as e:
         logger.exception(f'Some error occured! {e}', exc_info=True)
         return False
+
+##join us on Tg- @kissuXbots
+##movies bot - @kissu_movies_bot
